27ccd93e5066938bac79066b00bed18b5501db2c
= <=>
2026-06-22 20:12:32 +0530

    created 'creteuser' logic

---
+++ app/db/models.py
@@ -0,0 +1,29 @@
+from .session import get_db
+from fastapi import Depends,HTTPException,status
+from sqlalchemy.orm import Session
+from schemas import UserRegister
+from ..models import UserModal
+from app import models
+
+def createuser(user:UserRegister,db:Session = Depends(get_db)):
+    username = db.query(UserModal).filter(UserModal.username == user.username).first()
+    if username:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User already exists with that username")
+    
+    useremail = db.query(UserModal).filter(UserModal.email == user.email).first()
+    if useremail:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Account already exists with that email.")
+    
+    hash_password = hash_password(user.password)
+
+    new_user = UserModal(
+        name = user.name,
+        username = user.username,
+        email = user.email,
+        password = hash_password,
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
