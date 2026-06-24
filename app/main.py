68c2c342020f324d6d315c521c9b091485ca70ff
= <=>
2026-06-24 15:24:40 +0530

    added route to get the current user data

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from sqlalchemy.orm import Session
 from core.security import settings
+from db.models import router as auth_router
+from api.dependencies import get_current_user
 
 app = FastAPI(
     title="LibStream",
     description="This platform is design to serve the Library System",
     version=settings.version)
 
+app.include_router(auth_router,prefix="/api")
+
 @app.get("/")
 def homepage():
     return {'code':'200','message':'Hello,Wolrd!'}
+
+@app.get("/api/protected-data")
+async def get_secure_data(current_user: str = Depends(get_current_user)):
+    return {'message':f"from {current_user = } you've access to this data "}
