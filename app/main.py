1d16064f031e2cd2e96f1ce17955bf8226bb622e
= <=>
2026-06-28 20:01:19 +0530

    Included book opeartion router

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import Session
 from app.core.security import settings
 from app.db.auth import router as auth_router
 from app.api.dependencies import get_current_user
-
+from app.crud import router as book_operation
 app = FastAPI(
     title="LibStream",
     description="This platform is design to serve the Library System",
     version=settings.version)
 
 app.include_router(auth_router,prefix="/api")
+app.include_router(book_operation,prefix="/operation")
 
 @app.get("/")
 def homepage():
@@ -18,3 +19,4 @@ def homepage():
 @app.get("/api/protected-data")
 async def get_secure_data(current_user: str = Depends(get_current_user)):
     return {'message':f"from {current_user = } you've access to this data "}
+
