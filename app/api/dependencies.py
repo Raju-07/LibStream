06769a2c029c2ec8eb041c67eeda5ff471134add
= <=>
2026-06-29 19:50:50 +0530

    created a dependency to check for current user is admin or not

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 import jwt
+from app.models import UserModal
 
 
 oauth_schema = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
@@ -23,10 +24,8 @@ async def get_current_user(token: str = Depends(oauth_schema)) -> str:
     
     return username
 
-async def current_user_admin(token: str = Depends(oauth_schema)):
-    payload = jwt.decode(token,settings.secret_key,settings.algorithm)
-    admin = payload.get('admin')
-    if admin:
-        return {'code':'','message':"you're elegible to perform this operation"}
-    else:
-        return {'code':'401','message':'Unauthorized Action'}
+async def get_admin_user(current_user: UserModal = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Only admin user can perform this operation")
+    return current_user
