f0f8e6f4398f3e4786260f18f33c6d17cd32bdc1
= <=>
2026-07-03 15:40:20 +0530

    changes the sql query to async mode -> from db.query --> db.execute

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,13 +1,20 @@
+import uuid
+
 from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 import jwt
 from app.models import UserModal
+from app.db.session import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 
 oauth_schema = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
-async def get_current_user(token: str = Depends(oauth_schema)) -> str:
+async def get_current_user(token: str = Depends(oauth_schema),
+                           db: AsyncSession = Depends(get_db)) -> UserModal:
+    
     credential_exception = HTTPException(
         status_code= status.HTTP_401_UNAUTHORIZED,
         detail='could not validate credential',
@@ -16,16 +23,39 @@ async def get_current_user(token: str = Depends(oauth_schema)) -> str:
 
     try:
         payload = jwt.decode(token,settings.secret_key,settings.algorithm)
-        username = payload.get("username")
-        if username is None:
+        user_id = payload.get("sub")
+        if user_id is None:
             raise credential_exception
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,"Token has Expired")
+    
     except jwt.PyJWTError:
         raise credential_exception
     
-    return username
+    try:
+        user_id = uuid.UUID(user_id)
+    except ValueError:
+        raise credential_exception
+    
 
-async def get_admin_user(current_user: UserModal = Depends(get_current_user)):
+    result = await db.execute(select(UserModal).where(UserModal.id == user_id))
+    user = result.scalar_one_or_none()
+    if user is None or not user.is_active:
+        raise credential_exception
+    
+    return user
+
+async def admin_required(current_user : UserModal = Depends(get_current_user)) -> None:
     if not current_user.is_admin:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Only admin user can perform this operation")
+        raise HTTPException(
+            status.HTTP_403_FORBIDDEN,
+            "Admin Privileges Required")
+    
+
+async def get_admin_user(current_user: UserModal= Depends(get_current_user)) -> UserModal:
+    if not current_user.is_admin:
+        raise HTTPException(
+            status.HTTP_403_FORBIDDEN,
+            "Admin Privileges Required")
     return current_user
+    
