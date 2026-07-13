23af0cebd83c9904d76579ff649611ec6420ca86
= <=>
2026-07-13 08:03:02 +0530

    implement New Book Adding logic

--- app/crud.py
+++ app/crud.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends,APIRouter
 from app.schemas import BookRequest
 from app.db.session import get_async_db
+from app.models import BooksModal
 
-router = APIRouter(prefix='admin',tags=["Admin Operations"])
+router = APIRouter(prefix='/admin',tags=["Admin Operations"])
 
 @router.post('/add-book')
 async def add_book(
@@ -13,5 +14,11 @@ async def add_book(
     admin_user : AsyncSession = Depends(get_admin_user),
     db: AsyncSession = Depends(get_async_db)
     ):
-    
-    return book
+    try:
+        new_book = BooksModal(**book.model_dump())
+        db.add(new_book)
+        await db.commit()
+        await db.refresh(new_book)
+    except:
+        await db.rollback()
+    return new_book
