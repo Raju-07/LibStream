e418c3d46b0356d672b1398b16e60b5ca7717b20
= <=>
2026-07-16 14:32:23 +0530

    1. gracefully closing the db session 2. ban user endpoint created 3. unban user endpoint created 4. Handling Error Efficiently

--- app/admin_operations.py
+++ app/admin_operations.py
@@ -1,11 +1,10 @@
 from app.api.dependencies import admin_required,is_book_exists
 from sqlalchemy import select,and_,delete
-from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends,APIRouter,HTTPException,status
 from app.schemas import AddBookRequest,UpdateBookRequest
 from app.db.session import get_async_db
-from app.models import BooksModal
+from app.models import BooksModal,UserModal
 from app.db.books_user_operation import get_book_by_id
 
 router = APIRouter(prefix='/admin',tags=["Admin Operations"])
@@ -23,12 +22,14 @@ async def add_book(
         await db.refresh(new_book)
     except:
         await db.rollback()
+    finally:
+        await db.close()
 
     return new_book
 
 
 @router.delete("/delete-book/{id}")
-async def delete_book_by_id(id:int,
+async def delete_book_by_id(id:int = Depends(get_book_by_id),
                             db: AsyncSession = Depends(get_async_db),
                             _: None = Depends(admin_required)):
     
@@ -44,6 +45,8 @@ async def delete_book_by_id(id:int,
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Deletation Failed due to {e}"
         )
+    finally:
+        await db.close()
     
 
 @router.patch("/update-book/{id}",status_code=status.HTTP_200_OK)
@@ -72,3 +75,91 @@ async def update_book(book: UpdateBookRequest ,id: int = Depends(is_book_exists)
         await db.rollback()
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
                             f"Error while updating Book : {str(e)}")
+    
+    finally:
+        await db.close()
+    
+
+# Endpoint to ban User's
+@router.patch("/ban-user/{username}")
+async def ban_user(username: str,_ : None = Depends(admin_required),
+                   db: AsyncSession = Depends(get_async_db)):
+    try:
+        user = await db.execute(select(UserModal).where(UserModal.username == username))
+        user = user.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(status.HTTP_404_NOT_FOUND,
+                                f"{username} doesn't exists")
+        
+        # making the user inactive (kindda ban)
+        user.is_active = False
+        await db.commit()
+
+        return {
+            'code':200,
+            'message':f'{username} banned successfully',
+            'user': user
+        }
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        await db.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            f"Error while banning user: {e}")
+    finally:
+        await db.close()
+    
+# for unbanning the user
+
+@router.patch("/unban-user/{username}")
+async def unban_user(username: str, _ : None = Depends(admin_required),
+                     db: AsyncSession = Depends(get_async_db)):
+    
+    try:
+        user = await db.execute(
+            select(UserModal).where(UserModal.username == username)
+        )
+        user = user.scalar_one_or_none()
+
+        if user is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND,
+                                f"{username} doesn't exists")
+        
+        if not user.is_active:
+            user.is_active = True
+            await db.commit()
+            return {
+                'code':200,
+                'message':'User is Unbanned Now',
+                'user':{
+                    'id':user.id,
+                    'name':user.name,
+                    'username':user.username,
+                    'email':user.email,
+                    'active':user.is_active
+                }
+            }
+        else:
+            return {
+                'code':200,
+                'message': "user is already unbanned",
+                'user':{
+                    'id':user.id,
+                    'name':user.name,
+                    'username':user.username,
+                    'email':user.email,
+                    'active':user.is_active
+                }
+            }
+        
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        await db.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            f"Error while Unbanning user: {e}")
+    finally:
+        await db.close()
